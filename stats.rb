8bf0c158ee6d10072809a77f6d8073f3c428b5a3
Geoffrey Brown <gbrown@brodio.com>
2011-12-05 00:14:44 -0500

    add word size average

--- stats.rb
+++ stats.rb
@@ -1,5 +1,10 @@
 #!/usr/bin/env ruby
 
-words = File.read('stopwords.txt').split(',')
-puts "Number of words: " + words.size.to_s
+puts "Stats from the stopwords file"
+puts "-----------------------------"
 
+words = File.read('stopwords.txt').split(',')
+puts "Number of words: ".ljust(25) + words.size.to_s
+
+avg = words.inject(0.0) { |sum, w| sum + w.size.to_f } / words.size
+puts "Average word size: ".ljust(25) + avg.to_s
