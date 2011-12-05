ead0142fa30d307a9c833c5b164d3d710214f55f
Geoffrey Brown <gbrown@brodio.com>
2011-12-05 00:35:30 -0500

    =add count of vowel-started words

--- stats.rb
+++ stats.rb
@@ -8,3 +8,8 @@ puts "Number of words: ".ljust(25) + words.size.to_s
 
 avg = words.inject(0.0) { |sum, w| sum + w.size.to_f } / words.size
 puts "Average word size: ".ljust(25) + avg.to_s
+
+vowels = ['a','e','i','o','u']
+vcount = words.select { |word| vowels.include?(word[0,1]) }.size
+puts "Vowel starting words: ".ljust(25) + vcount.to_s
+
